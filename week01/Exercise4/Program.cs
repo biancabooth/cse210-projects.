1e71aa497ec19c0879d0fd64c2b1a0153baed3ed
biancabooth <bbooth@byupathway.edu>
2025-11-02 09:42:47 -0700

    pushing exercise no 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,56 @@
+using System.Data;
 using System;
-
+using System.Collections.Generic;
+using System.Numerics;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number = -1;
+        int sum = 0;
+        int highestNumber = -1;
+        List<int> numbers = new List<int>();
+        while (number != 0)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+
+            }
+        }
+        foreach (int i in numbers)
+        {
+            sum += i;
+            int oneNumber = i;
+            if (i > highestNumber)
+            {
+                highestNumber = i;
+            }
+        }
+
+        float average = (float)sum / numbers.Count;
+
+        Console.WriteLine($"The total is: {sum}");
+        Console.WriteLine($"The average is: {average}"); 
+        Console.WriteLine($"The highest number is: {highestNumber}");
     }
 }
+
+
+    
+
+
+
+
+
+
+
+    
+
+
+
