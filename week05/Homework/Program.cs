c7e1e27db2e263ac6d1b1486af41888b4ea09fd3
biancabooth <bbooth@byupathway.edu>
2025-11-29 09:45:34 -0700

    week05 Learning Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+        Assignment studentInfo1 = new Assignment ("Samuel Bennet","Multiplication");
+        Console.WriteLine(studentInfo1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment1 = new MathAssignment ("Roberto Rodriguez","Fractions","Section 7.3","Problem 8-19");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters","European History","The Cause of World War II");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,24 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    
+    public WritingAssignment(string studentName,string topic,string title ) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+    public void SetTitle(string title)
+    {
+       _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        string writingInformation = $"{GetTopic()} by {GetStudentName()}";
+        return writingInformation;
+    }
+}
