f5a3e30733b300727dd9e8f3af21fd8aa672cb17
biancabooth <bbooth@byupathway.edu>
2025-12-05 14:53:06 -0700

    shapes week06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square1 = new Square("blue",4);
+        Console.WriteLine(square1.GetColor());
+        Console.WriteLine(square1.GetArea());
+
+        Rectangle rectangle1 = new Rectangle("green",5,2);
+        Console.WriteLine(rectangle1.GetColor());
+        Console.WriteLine(rectangle1.GetArea());
+
+        Circle circle1 = new Circle("red", 5);
+        Console.WriteLine(circle1.GetColor());
+        Console.WriteLine(circle1.GetArea());
+
+        List<Shape> shapes1 = new List<Shape>();
+        shapes1.Add(square1);
+        shapes1.Add(rectangle1);
+        shapes1.Add(circle1);
+        foreach(Shape shape in shapes1)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+
+        }
+
+
+
+
     }
+
 }
---
+++ week06/Shapes/Square.cs
@@ -0,0 +1,17 @@
+public class Square : Shape
+{
+   private double _side;
+
+   public Square(string color, double side) : base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        double area = _side * 2;
+        return Math.Round(area,2);
+
+    }
+
+}
